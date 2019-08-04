5e66f8ac717c48d1a6b897a0b0a49e982ebeb402
rick427 <44168932+rick427@users.noreply.github.com>
2019-08-04 16:53:17 +0100

    added i.o for  animations

--- js/observers.js
+++ js/observers.js
@@ -1,20 +1,46 @@
 const header = document.querySelector("header");
 const sectionOne = document.querySelector(".home-intro");
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
+// NAVBAR INTERSECTION OBSERVER
 const sectionOptions = {
     rootMargin: "-200px 0px 0px 0px"
 };
-
 const sectionObserver = new IntersectionObserver((entries, sectionObserver) => {
     entries.forEach(entry => {
        if(!entry.isIntersecting){
            header.classList.add("nav-scrolled");
-           console.log(entry)
        }
        else{
            header.classList.remove("nav-scrolled");
        }
     })
 }, sectionOptions);
-
 sectionObserver.observe(sectionOne);
+
+//FADE IN ANIMATIONS INTERSECTION OBSERVER
+const appearOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -250px 0px'
+};
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+   entries.forEach(entry => {
+       if(!entry.isIntersecting){
+          return; 
+       }
+       else{
+           entry.target.classList.add('appear');
+           appearOnScroll.unobserve(entry.target);
+       }
+   })
+},appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
+
